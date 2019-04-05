ae1783edc4887a676fae28d696fe67f8afc79745
Görkem KARA <resulgorkemkara@gmail.com>
2019-04-05 21:58:05 +0300

    Initial commit

---
+++ app/src/main/java/gorkemkara/com/tr/havadurumu/WeatherResponse.java
@@ -0,0 +1,92 @@
+package gorkemkara.com.tr.havadurumu;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Görkem KARA on 05-04-2019.
+ */
+
+public class WeatherResponse {
+
+    @SerializedName("coord")
+    public Coord coord;
+    @SerializedName("sys")
+    public Sys sys;
+    @SerializedName("weather")
+    public ArrayList<Weather> weather = new ArrayList<Weather>();
+    @SerializedName("main")
+    public Main main;
+    @SerializedName("wind")
+    public Wind wind;
+    @SerializedName("rain")
+    public Rain rain;
+    @SerializedName("clouds")
+    public Clouds clouds;
+    @SerializedName("dt")
+    public float dt;
+    @SerializedName("id")
+    public int id;
+    @SerializedName("name")
+    public String name;
+    @SerializedName("cod")
+    public float cod;
+}
+
+class Weather {
+    @SerializedName("id")
+    public int id;
+    @SerializedName("main")
+    public String main;
+    @SerializedName("description")
+    public String description;
+    @SerializedName("icon")
+    public String icon;
+}
+
+class Clouds {
+    @SerializedName("all")
+    public float all;
+}
+
+class Rain {
+    @SerializedName("3h")
+    public float h3;
+}
+
+class Wind {
+    @SerializedName("speed")
+    public float speed;
+    @SerializedName("deg")
+    public float deg;
+}
+
+class Main {
+    @SerializedName("temp")
+    public float temp;
+    @SerializedName("humidity")
+    public float humidity;
+    @SerializedName("pressure")
+    public float pressure;
+    @SerializedName("temp_min")
+    public float temp_min;
+    @SerializedName("temp_max")
+    public float temp_max;
+}
+
+class Sys {
+    @SerializedName("country")
+    public String country;
+    @SerializedName("sunrise")
+    public long sunrise;
+    @SerializedName("sunset")
+    public long sunset;
+}
+
+class Coord {
+    @SerializedName("lon")
+    public float lon;
+    @SerializedName("lat")
+    public float lat;
+}
